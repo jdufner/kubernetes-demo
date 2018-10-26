62a10f8b4817603bdf172e8e3ce639c773e5c722
Jürgen Dufner <juergen@jdufner.de>
2018-10-26 20:59:29 +0200

    intial commit

---
+++ src/main/java/de/jdufner/kubernetes/demo/DemoController.java
@@ -0,0 +1,15 @@
+package de.jdufner.kubernetes.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+  @GetMapping("/")
+  public String helloWorld() {
+    return "Hello World!";
+  }
+
+}
